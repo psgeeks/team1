3ff227554295500d1f1927efe52a29ce6c98f099
gyunho <124599614+g0rnn@users.noreply.github.com>
2026-02-12 09:47:52 +0900

    2026-02-12

--- g0rnn/simulation/Sol12100.java
+++ g0rnn/simulation/Sol12100.java
@@ -1,4 +1,4 @@
-package beakjoon;
+package psg;
 
 import java.util.*;
 import java.io.*;
@@ -7,13 +7,7 @@ public class Sol12100 {
     // 5번 이동 & sout(만들 수 있는 가장 큰 블록)
     // 1 <= n <= 20
 
-    // idea. 2차원 배열 6개를 만들자. << 이렇게 안하면 메모리가 얼마나 튈지 모르겠음
-    // map[0][][] 은 맨 처음 한번도 이동안한 맵
-    // map[1][][] 은 한번 이동을 시도한 맵
-
-
-
-    static int[][][] map;
+    static int[][] map;
     static int n;
     static int max = Integer.MIN_VALUE;
     static int[][] offset = {{0, -1}, {1, 0}, {0, 1}, {-1, 0}};
@@ -21,71 +15,74 @@ public class Sol12100 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-        map = new int[6][n][n];
+        map = new int[n][n];
 
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < n; j++) map[0][i][j] = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < n; j++) map[i][j] = Integer.parseInt(st.nextToken());
         }
 
-        backtracking(0, 0);
+        backtracking(0, 0, map);
+        System.out.println(max);
     }
 
-    private static void backtracking(int depth, int curSum) {
+    private static void backtracking(int depth, int curBlock, int[][] curMap) {
         // 여기에 가지치기 한판
-
+    	
         if (depth == 5) {
-            max = Math.max(max, curSum);
+            max = Math.max(max, curBlock);
             return;
         }
 
         for (int dir = 0; dir < 4; dir++) {
-            // dir방향으로 한칸 땡겨 합침
-            // gravity
-            backtracking(depth+1, curSum + point);
-            // 복구??
+            Result result = move(dir, curMap);
+            backtracking(depth+1,  result.maxValue, result.next);
         }
     }
-
-    // 내 이론으론 안됨.. 홀수면 대체 어카노
-    private static void moveOne(int[][] prev, int[][] next, int dir) {
-        int cnt = n;
-        while (cnt-- > 0) {
-            // 한 행 or 열을 1칸씩 이동
-
-
-            int nr = row + offset[dir][1];
-            int nc = col + offset[dir][0];
-            if (nr < 0 || nr >= n || nc < 0 || nc >= n) continue;
-
-            if (prev[nr][nc] == prev[row][col]) {
-                next[nr][nc] = prev[row][col]*2;
-                next[row][col] = 0;
-
-            }
-            // 여기서 rol = nr; row += offset 을 한번 더하고 모듈러
-            // 그러면 사실상 두칸을 이동한건데, 그럼 홀수면 대참사가 남. 진짜로;
-        }
+    
+    // 0: 상, 1: 하, 2: 좌, 3: 우
+    private static Result move(int dir, int[][] board) {
+    	int[][] next = new int[n][n];
+    	int maxBlock = 0;
+    	
+    	for (int i = 0; i < n; i++) {
+    		List<Integer> target = new ArrayList<>();
+    		for (int j = 0; j < n; j++) {
+    			// 위쪽으로 이동하려면 상->하로 순회
+    			// 아래쪽으로 이동하려면 하->상으로 순회
+    			// 왼쪽으로 이동하려면 좌->우로 순회해야함.
+    			// 오른족으로 이동하려면 우->좌로 순회해야함. 나중에 리스트를 순회할 때 오른쪽 원소부터 merge됨.
+    			int val = (dir < 2) ? (dir == 0 ? board[j][i] : board[n-1-j][i]) // j가 변경되므로 상/하로 이동하기 위해선 map[j][]로 건드려야함
+    								: (dir == 2 ? board[i][j] : board[i][n-1-j]);// 좌/우로 이동하기위해 map[][j]로 건드려야함.
+    			
+    			if (val != 0) target.add(val); 
+    		}
+    		
+    		List<Integer> merged = new ArrayList<>();
+    		for (int j = 0; j < target.size(); j++) {
+    			if (j + 1 < target.size() && target.get(j).equals(target.get(j+1))) {
+    				merged.add(target.get(j)*2); // 합침
+    				j++; // 다음으로 넘김
+    			} else merged.add(target.get(j));
+    		}
+    		
+    		for (int j = 0; j < merged.size(); j++) {
+    			int val = merged.get(j);
+    			
+    			if (dir == 0) next[j][i] = val; // [j++][] 형태이므로 상->하 로 이동하며 값을 넣음 == 위로 이동
+    			else if (dir == 1) next[n-j-1][i] = val;
+    			else if (dir == 2) next[i][j] = val;
+    			else next[i][n-j-1] = val;
+    			
+    			maxBlock = Math.max(val, maxBlock);
+    		}
+    	}
+    	return new Result(next, maxBlock);
     }
-
-    // 일단 아래로 당겨봐
-    private static void draw(int[][] prev, int[][] next) {
-        for (int col = 0; col < n; col++) {
-            int wRow = n-1; // 밑에서 채움, 채울곳의 row
-
-            for (int row = n-1; row >= 0; row--) { // 땡길 녀석의 row
-                if (prev[row][col] == 0) continue;
-                if (wRow == row) continue;
-                // 땡길 녀석과 채울 곳이 같은 숫자일 때
-                if (prev[wRow][col] == prev[row][col]) {
-                    next[wRow][col] = prev[row][col]*2;
-                    wRow--;
-                } else {
-                    next[wRow][col] = prev[row][col]; // 걍 땡기면 wRow를 -1하면 안됨.
-                }
-            }
-
-            // 다 땡겼는데 wRow가 범위를 넘지못했으면 0으로 채움.
-        }
+    
+    static class Result {
+    	int[][] next;
+    	int maxValue;
+    	Result(int[][] board, int val) {next=board;maxValue=val;}
     }
 }
